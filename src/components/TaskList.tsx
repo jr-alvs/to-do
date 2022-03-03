4f6912f782a30892dba4c2c8e05ee10e432acbcc
Junior Alves <helio.alvs@gmail.com>
2022-03-03 17:27:43 -0300

    Challenge concludedgit add .

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,30 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
-  function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+  const handleCreateNewTask = () => {
+    if (newTaskTitle) {
+      const newTask = {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false,
+      }
+      setTasks(array => [...array, newTask]);
+      setNewTaskTitle('');
+    }
   }
 
-  function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+  const handleToggleTaskCompletion = (id: number) => {
+    const checkedTask = tasks.map(
+      task => task.id === id ? {
+        ...task,
+        isComplete: !task.isComplete
+      } : task);
+    setTasks(checkedTask);
   }
 
-  function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+  const  handleRemoveTask = (id: number) => {
+    const mapTasks = tasks.filter(task => task.id !== id);
+    setTasks(mapTasks);
   }
 
   return (
